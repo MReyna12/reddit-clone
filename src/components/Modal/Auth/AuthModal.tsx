1a6700117e4a8b938ad82e7db194971b3c2f3f96
Michael <mreyna12@live.com>
2023-02-23 11:33:43 -0600

    create modal state for login and signup buttons

---
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -0,0 +1,37 @@
+import { authModalState } from "@/atoms/authModalAtom";
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import React from "react";
+import { useRecoilState } from "recoil";
+
+const AuthModal: React.FC = () => {
+  const [modalState, setModalState] = useRecoilState(authModalState);
+
+  const handleClose = () => {
+    setModalState((prevValue) => ({
+      ...prevValue,
+      open: false,
+    }));
+  };
+
+  return (
+    <>
+      <Modal isOpen={modalState.open} onClose={handleClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Here is the modal body</ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default AuthModal;
